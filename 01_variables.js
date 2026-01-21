5c28a52042aceceaf27e85de9973037e01480b1e
codewithprince-max <princeaggarwal885@gmail.com>
2026-01-21 06:32:25 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1713;
+let rollNumber = "24032004053";
+var studentName = "Prince";
+studentCity = Hodal;
+//  accountId = 1713 // not changed
+
+studentName ="SHikha";
+rollNumber = 1317;
+studentCity = "Bangluru";
+ console.log(rollNumber);
+ console.table([studentName,rollNumber,studentCity])
