3945be49c9486b032963b5587d66a80bd5430abe
codewithprince-max <princeaggarwal885@gmail.com>
2026-01-21 09:19:08 +0000

    Conversion of datatypes

---
+++ 01_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = "13abd"
+//console.log(typeof score);
+
+let valueInNumber = Number(score); 
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+//"33" => 33
+//"33abc"=> NaN
+//true => / false => 0
+let loggedIN = null;
+ let booleanloggedIN = Boolean(loggedIN)
+ //console.log(booleanloggedIN);
+ 
+ //true => 1 / false => 0
+ //"" => false 
+ //"Prince" => true  
+
+ let somenumber = 33
+
+ let stringnumber = String(somenumber)
+ console.log(stringnumber);
+ console.log(typeof stringnumber );
+ 
