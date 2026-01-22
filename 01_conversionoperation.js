e06bc0c3179d0065952f20ba1688ecb22c4b5411
codewithprince-max <princeaggarwal885@gmail.com>
2026-01-22 07:37:16 +0000

    comparison and conversion of datatypes

--- 01_conversionoperation.js
+++ 01_conversionoperation.js
@@ -1,7 +1,7 @@
 let score = "13abd"
 //console.log(typeof score);
 
-let valueInNumber = Number(score); 
+let valueInNumber = Number(score);
 //console.log(valueInNumber);
 //console.log(typeof valueInNumber);
 
@@ -10,16 +10,36 @@ let valueInNumber = Number(score);
 //"33abc"=> NaN
 //true => / false => 0
 let loggedIN = null;
- let booleanloggedIN = Boolean(loggedIN)
- //console.log(booleanloggedIN);
- 
- //true => 1 / false => 0
- //"" => false 
- //"Prince" => true  
+let booleanloggedIN = Boolean(loggedIN)
+//console.log(booleanloggedIN);
 
- let somenumber = 33
+//true => 1 / false => 0
+//"" => false 
+//"Prince" => true  
 
- let stringnumber = String(somenumber)
- console.log(stringnumber);
- console.log(typeof stringnumber );
- 
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+//  console.log(stringnumber);
+//  console.log(typeof stringnumber ); 
+
+// ********************* Operations *************************
+
+let str1 = "Hello"
+let str2 = " Prince Aggarwal"
+let str3 = (str1 + str2)
+//  console.log(str3);
+//  console.log("1" + 2 );
+//  console.log(1 + "2");
+// console.log(1 + 3 + "2");
+// console.log(1 + "3" + "2");
+// console.log("1" + 2 + 2 );
+// console.log("1" + 2 + "1");
+// console.log(1 + "3" + 2);
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+
+
