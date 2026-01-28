3697d5b6cb9025b6552cd79c707f9ea6984712a7
codewithprince-max <princeaggarwal885@gmail.com>
2026-01-28 09:59:11 +0000

    numbers  and Math

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 1124.2866;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+//+++++++++++++++++++++++++++++ Mathematics +++++++++++++++++++++++++++++++++++
+// console.log(Math.round(4.6));
+// console.log(Math.abs(-10));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,3,45,5,1,9));
+// console.log(Math.max(2,4,5,9,4333));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) +1 );
+const max =10 
+const min = 20;
+console.log(Math.floor(Math.random() * (max - min +1 ) +min));
+
+
+
+
+
+
