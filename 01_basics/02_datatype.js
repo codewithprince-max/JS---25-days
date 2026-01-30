7d0c6beb7e8908d8c261b294040ac326d521bd0a
codewithprince-max <princeaggarwal885@gmail.com>
2026-01-30 14:20:19 +0530

    Add files via upload

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,12 @@
+"use strict"
+/*
+string => ""
+boolean => true/false
+null => stand alone value 
+number => 2 to power 53
+bigint => 
+ symbol => unique   
+unndefined =>
+*/
+console.log(typeof null);
+console.log(typeof undefined);
