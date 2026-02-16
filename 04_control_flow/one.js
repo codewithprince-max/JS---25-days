fd130c98c0a94669be30a1723db7e929e4146ff4
codewithprince-max <princeaggarwal885@gmail.com>
2026-02-16 16:49:30 +0530

    if else statement and logical operator

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// if 
+const isUserLoggedIn = true
+const temprature = 41
+if (temprature === 41) {
+    console.log("less than 50");
+}
+else{
+    console.log("greater than 50 ");
+}
+console.log("executed");
+// > , < , >= , <= , === ,  == , != , !==
+// const score = 200 ; 
+// if(score>100){
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+     
+// }
+//  console.log(`user power : ${power}`);
+const balance = 1000
+// if(balance>500) console.log("YEs is true") , console.log("test");
+
+// if (balance<500) {
+//       console.log("less than 500");
+// } else if(balance <750) {
+//       console.log("less than 750");
+// }else if(balance<900){
+//   console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+    
+// }
+const UserLoggedIn = true
+const debitCard = true
+const  LoggedInfromGoogle = false
+const  LoggedInfromemail = true
+ if (UserLoggedIn && debitCard ) {
+    console.log("You are eligilbe");
+    
+    
+ }
+ if ( LoggedInfromGoogle ||  LoggedInfromemail || true ) {
+    console.log("User logged In ");
+    
+    
+ }
