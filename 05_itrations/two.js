0b86396a7b2553eb7f65afbc5c24a91b02577553
codewithprince-max <princeaggarwal885@gmail.com>
2026-02-17 13:40:24 +0530

    do and do while loop and use this loop in array

---
+++ 05_itrations/two.js
@@ -0,0 +1,25 @@
+ 
+let index = 0 
+ while (index <=10) {
+    // console.log(`the value of index is : ${index}`);
+    index = index + 3
+ }
+ let myArray = [`flash`, ' blue','red']
+ let arr = 0
+ while (arr< myArray.length) {
+    // console.log(`the value of arr is ${myArray[arr]}`);
+    arr = arr + 1 
+    
+        
+    }
+    // let score = 1
+    // do {
+    //     console.log(`the value of score is ${score}`);
+    //     score ++
+    // } while (score<= 10);
+    let score = 11
+    do {
+        console.log(`the value of score is ${score}`);
+        score ++
+    } while (score<= 10);
+ 
