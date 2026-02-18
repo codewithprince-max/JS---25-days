388d0a7081af18b247254bb1e60438b561bd6689
codewithprince-max <princeaggarwal885@gmail.com>
2026-02-18 16:39:39 +0530

    maps

---
+++ 05_itrations/seven.js
@@ -0,0 +1,25 @@
+const myNumbers = [ 1,2,3,4,5,6,7,8,9,10 ]
+// const nums = myNumbers.map((arr)=>  {return arr + 10}  )
+// console.log(nums);
+//  const nums = []
+//  myNumbers.forEach((num) => { // 👉 forEach() kuch return nahi karta
+//     nums.push(num + 20)
+
+// })
+// console.log(nums);
+ 
+const nums = myNumbers
+.map((num )=> num *10)
+.map((num)=> num +2)
+.filter((num )=> num>=50)
+console.log(nums);
+
+
+
+
+
+
+
+
+
+
