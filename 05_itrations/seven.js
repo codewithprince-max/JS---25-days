e93776ad73e33ff17ac4d08e9e6dbcae6db6a5f9
codewithprince-max <princeaggarwal885@gmail.com>
2026-02-21 14:51:48 +0530

    maps

--- 05_itrations/seven.js
+++ 05_itrations/seven.js
@@ -7,7 +7,7 @@ const myNumbers = [ 1,2,3,4,5,6,7,8,9,10 ]
 
 // })
 // console.log(nums);
- 
+// this is called chaining in maps() 
 const nums = myNumbers
 .map((num )=> num *10)
 .map((num)=> num +2)
